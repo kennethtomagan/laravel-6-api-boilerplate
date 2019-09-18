c7065db912d11655b6d441b624e9b3b05012ced7
kenneth <kenneth.tomagan123@gmail.com>
2019-09-18 16:29:05 +0800

    authentication

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -5,6 +5,10 @@ namespace App\Http\Controllers\Auth;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginFormRequest;
+use JWTAuth;
+use App\User;
+use Carbon\Carbon;
+use Illuminate\Hashing\BcryptHasher;
 
 class AuthController extends Controller
 {
@@ -17,6 +21,34 @@ class AuthController extends Controller
      */
     public function login(LoginFormRequest $request)
     {
-        dd('asdasd');
+        $user = User::where('email', $request->email)->first();
+
+        if(!$user) return response()->json(['error' => 'User not found.'], 404);
+
+        // Account Validation
+        if (!(new BcryptHasher)->check($request->input('password'), $user->password)) {
+
+            return response()->json(['error' => 'Email or password is incorrect. Authentication failed.'], 401);
+        }
+
+        // Login Attempt
+        $credentials = $request->only('email', 'password');
+
+        try {
+
+            // JWTAuth::factory()->setTTL(40320); // Expired Time 28days
+
+            if (! $token = JWTAuth::attempt($credentials, ['exp' => Carbon::now()->addDays(28)->timestamp])) {
+
+                return response()->json(['error' => 'invalid_credentials'], 401);
+
+            }
+        } catch (JWTException $e) {
+
+            return response()->json(['error' => 'could_not_create_token'], 500);
+
+        }
+
+        return response()->json(compact('token', 'user'));
     }
 }
--- app/User.php
+++ app/User.php
@@ -5,8 +5,9 @@ namespace App;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use Notifiable;
 
@@ -28,12 +29,24 @@ class User extends Authenticatable
         'password', 'remember_token',
     ];
 
+    
     /**
-     * The attributes that should be cast to native types.
+     * Get the identifier that will be stored in the subject claim of the JWT.
      *
-     * @var array
+     * @return mixed
      */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
 }
