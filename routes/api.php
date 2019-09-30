8b04dea9b6e661d93c1170e767e500b636f9771c
kenneth <kenneth.tomagan123@gmail.com>
2019-09-30 10:00:33 +0800

    another update

--- routes/api.php
+++ routes/api.php
@@ -13,40 +13,46 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::group(['prefix' => 'v1'], function () {
+
+Route::get('/', function () {
+    return [
+        'app' => 'Laravel 6 API Boilerplate',
+        'version' => '1.0.0',
+    ];
+});
+
+
+Route::group(['namespace' => 'Auth'], function () {
+
+    Route::post('auth/login', ['as' => 'login', 'uses' => 'AuthController@login']);
+
+    Route::post('auth/register', ['as' => 'register', 'uses' => 'RegisterController@register']);
+    // Send reset password mail
+    Route::post('auth/recovery', 'ForgotPasswordController@sendPasswordResetLink');
+    // handle reset password form process
+    Route::post('auth/reset', 'ResetPasswordController@callResetPassword');
+
+});
+
+Route::group(['middleware' => ['jwt', 'jwt.auth']], function () {
+
+    Route::group(['namespace' => 'Profile'], function () {
+
+        Route::get('profile', ['as' => 'profile', 'uses' => 'ProfileController@me']);
+
+        Route::put('profile', ['as' => 'profile', 'uses' => 'ProfileController@update']);
+
+        Route::put('profile/password', ['as' => 'profile', 'uses' => 'ProfileController@updatePassword']);
+
+    });
 
     Route::group(['namespace' => 'Auth'], function () {
 
-        Route::post('auth/login', ['as' => 'login', 'uses' => 'AuthController@login']);
-
-        Route::post('auth/register', ['as' => 'register', 'uses' => 'RegisterController@register']);
-        // Send reset password mail
-        Route::post('auth/recovery', 'ForgotPasswordController@sendPasswordResetLink');
-        // handle reset password form process
-        Route::post('auth/reset', 'ResetPasswordController@callResetPassword');
+        Route::post('logout', ['as' => 'logout', 'uses' => 'LogoutController@logout']);
 
     });
 
-    Route::group(['middleware' => ['jwt', 'jwt.auth']], function () {
-
-        Route::group(['namespace' => 'Profile'], function () {
-
-            Route::get('profile', ['as' => 'profile', 'uses' => 'ProfileController@me']);
-
-            Route::put('profile', ['as' => 'profile', 'uses' => 'ProfileController@update']);
-
-            Route::put('profile/password', ['as' => 'profile', 'uses' => 'ProfileController@updatePassword']);
-
-        });
-
-        Route::group(['namespace' => 'Auth'], function () {
-    
-            Route::post('logout', ['as' => 'logout', 'uses' => 'LogoutController@logout']);
-    
-        });
-
-    });
-
-
-    
 });
+
+
+    
