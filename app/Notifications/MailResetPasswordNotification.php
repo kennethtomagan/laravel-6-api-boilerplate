867255aafc158eb56d7ef8b6fd1e0d85087cfb5c
kenneth <kenneth.tomagan123@gmail.com>
2019-11-05 18:26:45 +0800

    light changes

--- app/Notifications/MailResetPasswordNotification.php
+++ app/Notifications/MailResetPasswordNotification.php
@@ -36,11 +36,11 @@ class MailResetPasswordNotification extends ResetPassword
      */
     public function toMail($notifiable)
     {
-        $link = config('url.frontend_url') . '?token=' . $this->token;
+        $frontendLink = config('url.frontend_url') . '/password/reset?token=' . $this->token;
         return ( new MailMessage )
             ->subject( 'Reset Password Notification' )
             ->line( "Hello! You are receiving this email because we received a password reset request for your account." )
-            ->action( 'Reset Password', $link )
+            ->action( 'Reset Password', $frontendLink )
             ->line( "This password reset link will expire in ".config('auth.passwords.users.expire')." minutes" )
             ->line( "If you did not request a password reset, no further action is required." );    }
     /**
